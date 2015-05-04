a1a8ae26f83878a9e25da6c19065451db12cb7e8
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-05-04 13:31:50 -0700

    adding all the post stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( refinery/blog/backend.css )
+Rails.application.config.assets.precompile += %w( refinery/blog/ui-lightness/jquery-ui-1.8.13.custom.css )
+Rails.application.config.assets.precompile += %w( refinery/blog/frontend.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
